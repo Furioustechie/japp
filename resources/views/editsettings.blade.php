ecc6920a812d30811bbb3e4a5a751df0e591771b
AAN <aan370@gmail.com>
2018-12-05 17:29:33 +0600

    Added try catch to settings controller

--- resources/views/editsettings.blade.php
+++ resources/views/editsettings.blade.php
@@ -17,65 +17,13 @@
 
         Tip 2: you can also add an image using data-image tag
     -->
-    <div class="logo">
-        <a href="#" class="simple-text logo-normal">
+    <div class="logo" >
+        <a href="#" class="simple-text logo-normal " >
             Jail Appeal
         </a>
       </div>
       <div class="sidebar-wrapper">
-        <ul class="nav">
-          <li class="nav-item">
-            <a class="nav-link" href="/dashboard">
-              <i class="material-icons">dashboard</i>
-              <p>Dashboard</p>
-            </a>
-          </li>
-          <li class="nav-item ">
-            <a class="nav-link" href="/appeals">
-              <i class="material-icons">person</i>
-              <p>Appeal List</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/appealForm">
-              <i class="material-icons">content_paste</i>
-              <p>Appeal Now</p>
-            </a>
-          </li>
-          
-        
-         <!-- Dropdown -->
-         <li> <a class="nav-link" id="DropdownMenuLink" data-toggle="collapse"  aria-expanded="false" data-target="#submenu1"> <i class="material-icons">settings_applications</i>Settings</a>
-          <ul class="collapse primary" id="submenu1" role="menu" aria-labelledby="DropdownMenuLink">
-                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#modalPrisonForm">Add New Prison Name</a>
-                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#modalSentenceForm">Add New Sentence Name</a>
-                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#modalCourtForm">Add New Court Name</a>
-                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#modalOffenceForm">Add New Offence Name</a>
-                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#modalStatusForm">Add New Status</a>
-                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#centralModalSuccess">Another One</a>
-          </ul>
-        </li>
- 
-      
-          <li class="nav-item active">
-            <a class="nav-link" href="/editsettings">
-              <i class="material-icons">edit</i>
-              <p>Edit Settings</p>
-            </a>
-          </li>
-          <li class="nav-item ">
-            <a class="nav-link" href="./map.html">
-              <i class="material-icons">location_ons</i>
-              <p>Maps</p>
-            </a>
-          </li>
-          <li class="nav-item ">
-            <a class="nav-link" href="./notifications.html">
-              <i class="material-icons">notifications</i>
-              <p>Notifications</p>
-            </a>
-          </li>
-          </ul>
+          @include('inc.sidenav')
       </div>
     </div>
     <div class="main-panel">
@@ -173,7 +121,7 @@
                                 
                                   <td class="text-right"><button type="button" rel="tooltip" title="Edit Record" class="btn btn-primary btn-link btn-sm" data-toggle="modal" data-target="#sentence_{{$sentence_data->id}}">
                                           <i class="material-icons">edit</i>
-                                  <a href="editsettings/sentence_name_destroy/{{$sentence_data->id}}" onclick="confirm('Are you Sure ??');"><button type="button" rel="tooltip" title="Delete Record" class="btn btn-primary btn-link btn-sm" >
+                                  <a href="editsettings/sentence_name_destroy/{{$sentence_data->id}}" onclick="if(!confirm('Are you Sure to DELETE?')){return false;}"><button type="button" rel="tooltip" title="Delete Record" class="btn btn-primary btn-link btn-sm" >
                                       <i class="material-icons">delete</i></a>
                                     
                                   </td>
@@ -250,7 +198,7 @@
                                       
                                         <td class="text-right"><button type="button" rel="tooltip" title="Edit Record" class="btn btn-primary btn-link btn-sm" data-toggle="modal" data-target="#prison_{{$data->id}}">
                                                 <i class="material-icons">edit</i>
-                                        <a href="editsettings/prison_name_destroy/{{$data->id}}" onclick="if (!confirm('Are you sure?')) { return false }""><button type="button" rel="tooltip" title="Delete Record" class="btn btn-primary btn-link btn-sm" >
+                                        <a href="editsettings/prison_name_destroy/{{$data->id}}" onclick="if(!confirm('Are you Sure to DELETE?')){return false;}"><button type="button" rel="tooltip" title="Delete Record" class="btn btn-primary btn-link btn-sm" >
                                             <i class="material-icons">delete</i></a>
                                           
                                         </td>
@@ -327,7 +275,7 @@
                                     
                                       <td class="text-right"><button type="button" rel="tooltip" title="Edit Record" class="btn btn-primary btn-link btn-sm" data-toggle="modal" data-target="#court_{{$court_data->id}}">
                                               <i class="material-icons">edit</i>
-                                      <a href="editsettings/court_name_destroy/{{$court_data->id}}" onclick="confirm('Are you Sure ??');"><button type="button" rel="tooltip" title="Delete Record" class="btn btn-primary btn-link btn-sm" >
+                                      <a href="editsettings/court_name_destroy/{{$court_data->id}}" onclick="if(!confirm('Are you Sure to DELETE?')){return false;}"><button type="button" rel="tooltip" title="Delete Record" class="btn btn-primary btn-link btn-sm" >
                                           <i class="material-icons">delete</i></a>
                                         
                                       </td>
@@ -404,7 +352,7 @@
                                           
                                             <td class="text-right"><button type="button" rel="tooltip" title="Edit Record" class="btn btn-primary btn-link btn-sm" data-toggle="modal" data-target="#offence_{{$offence_data->id}}">
                                                     <i class="material-icons">edit</i>
-                                            <a href="editsettings/offence_name_destroy/{{$offence_data->id}}" onclick="confirm('Are you Sure ??');"><button type="button" rel="tooltip" title="Delete Record" class="btn btn-primary btn-link btn-sm" >
+                                            <a href="editsettings/offence_name_destroy/{{$offence_data->id}}" onclick="if(!confirm('Are you Sure to DELETE?')){return false;}"><button type="button" rel="tooltip" title="Delete Record" class="btn btn-primary btn-link btn-sm" >
                                                 <i class="material-icons">delete</i></a>
                                               
                                             </td>
@@ -481,7 +429,7 @@
                                               
                                                 <td class="text-right"><button type="button" rel="tooltip" title="Edit Record" class="btn btn-primary btn-link btn-sm" data-toggle="modal" data-target="#status_{{$status_data->id}}">
                                                         <i class="material-icons">edit</i>
-                                                <a href="editsettings/offence_name_destroy/{{$status_data->id}}" onclick="confirm('Are you Sure ??');"><button type="button" rel="tooltip" title="Delete Record" class="btn btn-primary btn-link btn-sm" >
+                                                <a href="editsettings/status_name_destroy/{{$status_data->id}}" onclick="if(!confirm('Are you Sure to DELETE?')){return false;}"><button type="button" rel="tooltip" title="Delete Record" class="btn btn-primary btn-link btn-sm" >
                                                     <i class="material-icons">delete</i></a>
                                                   
                                                 </td>
---
+++ resources/views/inc/sidenav.blade.php
@@ -0,0 +1,53 @@
+<ul class="nav">
+        <li class="nav-item">
+          <a class="nav-link" href="/dashboard">
+            <i class="material-icons">dashboard</i>
+            <p>Dashboard</p>
+          </a>
+        </li>
+        <li class="nav-item ">
+          <a class="nav-link" href="/appeals">
+            <i class="material-icons">person</i>
+            <p>Appeal List</p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/appealForm">
+            <i class="material-icons">content_paste</i>
+            <p>Appeal Now</p>
+          </a>
+        </li>
+        
+      
+       <!-- Dropdown -->
+       <li class="nav-item"> <a class="nav-link" href="#" id="DropdownMenuLink" data-toggle="collapse"  aria-expanded="false" data-target="#submenu1"> <i class="material-icons">settings_applications</i>Settings</a>
+        <ul class="collapse primary" id="submenu1" role="menu" aria-labelledby="DropdownMenuLink">
+                  <a class="dropdown-item" href="#" data-toggle="modal" data-target="#modalPrisonForm">Add New Prison Name</a>
+                  <a class="dropdown-item" href="#" data-toggle="modal" data-target="#modalSentenceForm">Add New Sentence Name</a>
+                  <a class="dropdown-item" href="#" data-toggle="modal" data-target="#modalCourtForm">Add New Court Name</a>
+                  <a class="dropdown-item" href="#" data-toggle="modal" data-target="#modalOffenceForm">Add New Offence Name</a>
+                  <a class="dropdown-item" href="#" data-toggle="modal" data-target="#modalStatusForm">Add New Status</a>
+                  <a class="dropdown-item" href="#" data-toggle="modal" data-target="#centralModalSuccess">Another One</a>
+        </ul>
+      </li>
+
+    
+        <li class="nav-item ">
+          <a class="nav-link" href="/editsettings">
+            <i class="material-icons">edit</i>
+            <p>Edit Settings</p>
+          </a>
+        </li>
+        <li class="nav-item ">
+          <a class="nav-link" href="./map.html">
+            <i class="material-icons">location_ons</i>
+            <p>Maps</p>
+          </a>
+        </li>
+        <li class="nav-item ">
+          <a class="nav-link" href="./notifications.html">
+            <i class="material-icons">notifications</i>
+            <p>Notifications</p>
+          </a>
+        </li>
+        </ul>
